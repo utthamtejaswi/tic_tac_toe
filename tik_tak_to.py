afb1d997f7d0571d11d1b690c5549e34d858d147
Uttham T H <utthamtejaswi@gmail.com>
2021-04-07 19:56:06 +0530

    this is the actual gui based game where all the algorithm is placed and also the code .

---
+++ tik_tak_to.py
@@ -0,0 +1,76 @@
+import pygame  
+import stats_tick
+
+pygame.init()  
+screen = pygame.display.set_mode((600,600))  
+done = False
+count=0
+class stats():
+        def __init__(self):
+                self.index_dict={(0,0):(100,100), (0,1):(300,100), (0,2):(500,100),
+                                 (1,0):(100,300), (1,1):(300,300), (1,2):(500,300),
+                                 (2,0):(100,500), (2,1):(300,500), (2,2):(500,500)}
+
+                self.box_dict={(0,0):(1), (0,1):(2), (0,2):(3),
+                               (1,0):(4), (1,1):(5), (1,2):(6),
+                               (2,0):(7), (2,1):(8), (2,2):(9)}
+
+        def return_column(self,pos):
+                self.pos=pos
+                if(self.pos[0]<=200):
+                        return(0)
+                if(self.pos[0]<=400):
+                        return(1)
+                if(self.pos[0]<=600):
+                        return(2)
+
+        def return_row(self,pos):
+                self.pos=pos
+                if(self.pos[1]<=200):
+                        return(0)
+                if(self.pos[1]<=400):
+                        return(1)
+                if (self.pos[1]<=600):
+                        return(2)
+        def lines(self):
+                pygame.draw.line(screen, (255,255,255), (200,0), (200,600),5)
+                pygame.draw.line(screen, (255,255,255), (400,0), (400,600),5)
+                pygame.draw.line(screen, (255,255,255), (0,200), (600,200),5)
+                pygame.draw.line(screen, (255,255,255), (0,400), (600,400),5)
+
+
+obj1=stats()
+obj2=stats_tick.tik_tak_toe_game()
+while not done:
+        obj1.lines()
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        quit()
+                        exit()
+                        
+                if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_r:
+                                screen.fill((0,0,0))
+                                count=0
+                                obj2.clear_all()
+                                
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                        pos =list(pygame.mouse.get_pos())
+                        pos1=75
+                        column=obj1.return_column(pos)
+                        row=obj1.return_row(pos)
+                        box=obj1.index_dict[(row,column)]
+                        box_num=obj1.box_dict[(row,column)]
+                        if (count%2==0):
+                                you_are="o"
+                                pygame.draw.circle(screen,(255,255,255),(box[0],box[1]),pos1,5)
+                        else:
+                                you_are="x"
+                                pygame.draw.line(screen,(255,255,255),(box[0]-pos1,box[1]-pos1),(box[0]+pos1,box[1]+pos1),5)
+                                pygame.draw.line(screen,(255,255,255),(box[0]-pos1,box[1]+pos1),(box[0]+pos1,box[1]-pos1),5)
+                        count+=1
+                        obj2.make_move(you_are,box_num)
+                        if(obj2.check_game_over()):
+                                print(you_are+" wins")
+        pygame.display.flip()
+
